43dfff1f155364cc345efa0d3f60f1fe7e00668d
CSCEdarraghj <darraghjackson04@gmail.com>
2024-12-04 12:05:00 -0500

    Add files via upload

---
+++ Lab01.java
@@ -0,0 +1,30 @@
+//AndrewJacksonDarragh
+public class Lab01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Drawing the initial shape:");
+		
+		
+		System.out.println(" _   _");
+		
+        System.out.println("( `^`))");
+		System.out.println("|    ||");
+		System.out.println("|    ||");
+		System.out.println("'----'`");
+		
+		System.out.println("Displaying 2 copies of the shape that are horizontally adjacent:");
+		
+		
+		System.out.println(" _  _  " + "\t"+" _   _");
+		System.out.println("( `^`))" + "\t"+"( `^`))");
+		System.out.println("|    ||" + "\t"+"|    ||");
+		System.out.println("|    ||" + "\t"+"|    ||");
+		System.out.println("'----'`" + "\t"+"'----'`");
+		
+		
+		
+	}
+
+}
